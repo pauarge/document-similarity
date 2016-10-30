b7f2f7a5f082f8754b335b8cfc2420b8aaa71d38
Pau Argelaguet <pau@outlook.com>
2016-10-30 16:33:08 +0100

    Reading from external files

--- jaccard-similarity/main.cpp
+++ jaccard-similarity/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <set>
 
 using namespace std;
@@ -32,11 +33,43 @@ double get_jaccard(const string &doc1, const string &doc2) {
 }
 
 
-int main() {
+int main(int argc, char *argv[]) {
 
-    string doc1 = "Hola em dic pau";
-    string doc2 = "Hola em dic Manel";
+    if (argc == 3) {
+        ifstream docfile1, docfile2;
 
-    cout << get_jaccard(doc1, doc2) << endl;
+        docfile1.open(argv[1]);
+        docfile2.open(argv[2]);
+
+        if (not docfile1.is_open()) {
+            cout << "Could not read first file." << endl;
+            return -1;
+        }
+
+        if (not docfile2.is_open()) {
+            cout << "Could not read second file." << endl;
+            return -1;
+        }
+
+        string doc1, doc2;
+
+        docfile1.seekg(0, ios::end);
+        doc1.reserve(docfile1.tellg());
+        docfile1.seekg(0, ios::beg);
+
+        docfile2.seekg(0, ios::end);
+        doc2.reserve(docfile2.tellg());
+        docfile2.seekg(0, ios::beg);
+
+        doc1.assign((istreambuf_iterator<char>(docfile1)), istreambuf_iterator<char>());
+        doc2.assign((istreambuf_iterator<char>(docfile2)), istreambuf_iterator<char>());
+
+        docfile1.close();
+        docfile2.close();
+
+        cout << get_jaccard(doc1, doc2) << endl;
+    } else {
+        cout << "Invalid number of arguments" << endl;
+    }
 
 }
