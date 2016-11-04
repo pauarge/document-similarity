1bb5e0d3dfbba67445c253d663a20bbcd105f40a
Pau Argelaguet <pau@outlook.com>
2016-11-04 22:02:55 +0100

    Implemented jaccard similarity with hashing

--- src/classes/Comparator.cpp
+++ src/classes/Comparator.cpp
@@ -1,19 +1,20 @@
 #include "Comparator.hpp"
 
 Comparator::Comparator(Document *doc1, Document *doc2) {
-
+    this->doc1 = doc1;
+    this->doc2 = doc2;
 }
 
 
 double Comparator::get_jaccard_similarity() {
-    set<string> shingles1 = doc1->get_shingles(KSHINGLES);
-    set<string> shingles2 = doc2->get_shingles(KSHINGLES);
+    set<unsigned long> shingles1 = doc1->get_hashed_shingles(KSHINGLES);
+    set<unsigned long> shingles2 = doc2->get_hashed_shingles(KSHINGLES);
 
-    set<string> shingles_intersect;
+    set<unsigned long> shingles_intersect;
     set_intersection(shingles1.begin(), shingles1.end(), shingles2.begin(), shingles2.end(),
                      inserter(shingles_intersect, shingles_intersect.begin()));
 
-    set<string> shingles_union;
+    set<unsigned long> shingles_union;
     set_union(shingles1.begin(), shingles1.end(), shingles2.begin(), shingles2.end(),
               inserter(shingles_union, shingles_union.begin()));
 
