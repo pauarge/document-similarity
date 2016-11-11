0d732f294657ed02a8f61f41805b7ad6aac5bc43
Pau Argelaguet <pau@outlook.com>
2016-11-11 21:35:21 +0100

    Added fast way of checking deviation

--- src/main.cpp
+++ src/main.cpp
@@ -45,9 +45,8 @@ int main(int argc, char *argv[]) {
 
         begin = clock();
         cout << "Jaccard similarity" << endl;
-        vector<vector<double>> D(docs.size(), vector<double>(docs.size()));
-        D = comparator.get_jaccard_similarity();
-        for (vector<double> V : D) {
+        vector<vector<double>> jaccard_res = comparator.get_jaccard_similarity();
+        for (vector<double> V : jaccard_res) {
             for (double d : V) {
                 cout << d << " ";
             }
@@ -57,8 +56,8 @@ int main(int argc, char *argv[]) {
 
         begin = clock();
         cout << "Minhash similarity" << endl;
-        D = comparator.get_minhash_similarity();
-        for (vector<double> V : D) {
+        vector<vector<double>> minhash_res = comparator.get_minhash_similarity();
+        for (vector<double> V : minhash_res) {
             for (double d:V) {
                 cout << d << " ";
             }
@@ -68,14 +67,23 @@ int main(int argc, char *argv[]) {
 
         begin = clock();
         cout << "LSH similarity" << endl;
-        D = comparator.get_lsh_similarity();
-        for (vector<double> v : D) {
-            for (double x : v) {
+        vector<vector<double>> lsh_res = comparator.get_lsh_similarity();
+        for (vector<double> V : lsh_res) {
+            for (double x : V) {
                 cout << x << " ";
             }
             cout << endl;
         }
         print_time(begin);
+
+        float diff = 0;
+        for (int i = 0; i < docs.size(); i++) {
+            for (int j = 0; j < docs.size(); j++) {
+                diff += abs(jaccard_res[i][j] - minhash_res[i][j]);
+            }
+        }
+        cout << "Precision total deviation (between Jaccard and Minhash): " << diff << endl;
+
 
     } else {
         cout << "Invalid number of arguments" << endl;
