8d0ea829177fce94b96883e337dacfd8f4f714b6
Pau Argelaguet <pau@outlook.com>
2016-11-15 17:51:24 +0100

    Cool UX on experiments

--- src/mainExperiments.cpp
+++ src/mainExperiments.cpp
@@ -1,18 +1,38 @@
 #include "classes/Comparator.hpp"
 #include "classes/Experiments.hpp"
-#include "utils/utils.hpp"
+
 
 int main(int argc, char *argv[]) {
 
     if (argc == 2) {
 
         vector<Document *> docs = get_docs_from_path(argv[1]);
-        Experiments experiment = Experiments();
-        /*
-        experiment.experiment_kshingles(docs);
         Comparator comparator = Comparator(docs);
-        experiment.experiment_parametresLSH(comparator);*/
-        experiment.experiment_hashed_shingles(docs);
+        Experiments experiment = Experiments();
+
+        cout << "Select experiment: " << endl;
+        cout << "1 - KSHINGLES" << endl;
+        cout << "2 - LSH params" << endl;
+        cout << "3 - Hashed shingles" << endl;
+
+        unsigned e;
+
+        while (cin >> e) {
+            switch (e) {
+                case 1:
+                    experiment.experiment_kshingles(docs);
+                    break;
+                case 2:
+                    experiment.experiment_parametres_lsh(comparator);
+                    break;
+                case 3:
+                    experiment.experiment_hashed_shingles(docs);
+                    break;
+                default:
+                    cout << "Command not defined" << endl;
+                    break;
+            }
+        }
 
     } else {
         cout << "Invalid number of arguments" << endl;
